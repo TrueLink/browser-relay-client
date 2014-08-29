1a578e1de2f5b89ba06ca1893590bb63c2a7a90e
Evgenus <chernyshov.eugene@gmail.com>
2014-08-30 00:00:25 +0300

    removing built files from repository

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-﻿export import event = require("./event");
-export import protocol = require("./protocol");
-export import connectionManager = require("./connection-manager");
-export import routingTable = require("./routing-table");
