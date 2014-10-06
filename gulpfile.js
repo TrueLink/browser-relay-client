74d239a1579e1d6379deaf8d93385531fafe92b3
Evgenus <chernyshov.eugene@gmail.com>
2014-10-06 11:23:02 +0300

    bundling removed

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,11 @@ gulp.task('default', function(done) {
     runSequence(
         "clean", 
         "compile", 
-        "bundle", 
-        "finalize",
         done);
 });
 
 gulp.task('clean', function () {
     return gulp.src([
-        'dist/**/*.js',
-        'dist/**/*.map',
         'lib/**/*.js',
         'lib/**/*.d.ts',
         ], { read: false })
@@ -43,47 +39,14 @@ gulp.task('compile', function () {
             ])
         .pipe(compiler)
         ;
-    
-    var filterPatterns = [
-        '**/event.*',
-        '**/protocol.*',
-        '**/connection-manager.*',
-        '**/routing.*',
-        '**/index.*',
-        ];
-
-    var dtsLibFilter = gulpFilter(filterPatterns);
-    var jsLibFilter = gulpFilter(filterPatterns);
 
     result.dts
-        .pipe(dtsLibFilter)
         .pipe(gulp.dest('lib'))
-        .pipe(dtsLibFilter.restore())
-        .pipe(gulp.dest('dist'))
         ;
 
     return result.js
-        .pipe(jsLibFilter)
         .pipe(gulp.dest('lib'))
-        .pipe(jsLibFilter.restore())
-        .pipe(gulp.dest('temp'))
         ;
-});
-
-gulp.task('bundle', function () {
-    return gulp.src("temp/app.js")
-        .pipe(browserify({
-            insertGlobals: true,
-            debug: true,
-        }))
-        .pipe(rename("index.js"))
-        .pipe(gulp.dest('dist'))
-        ;
-});
-
-gulp.task('finalize', function() {
-    return gulp.src("temp")
-        .pipe(rimraf({ force: true }));
 });
 
 // gulp.task('spa', ['compile'], function () {
