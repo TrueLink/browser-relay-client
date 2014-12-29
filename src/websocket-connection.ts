e3115f032284826809af647716c5d01b5e3e07a0
Evgenus <chernyshov.eugene@gmail.com>
2014-12-29 18:21:03 +0200

    trying w3c

--- src/websocket-connection.ts
+++ src/websocket-connection.ts
@@ -1,7 +1,6 @@
 ﻿import connection = require("./connection");
 import protocol = require("./protocol");
 import event = require("./event");
-import WebSocketClient = require('websocket');
 
 export interface WebSocketConnectionAPI extends connection.ConnectionAPI {
     onOpen: event.Event<Event>;
@@ -9,78 +8,7 @@ export interface WebSocketConnectionAPI extends connection.ConnectionAPI {
     onClose: event.Event<CloseEvent>;
 }
 
-export class NodeWebSocketConnection extends connection.Connection {
-
-    private _address: string;
-    private _webSocket: WebSocketClient.client;
-    private _webSocketConnection: WebSocketClient.connection;
-
-    public onOpen: event.Event<any> = new event.Event<any>();
-    public onError: event.Event<ErrorEvent> = new event.Event<ErrorEvent>();
-    public onClose: event.Event<CloseEvent> = new event.Event<CloseEvent>();
-
-    public getEndpoint(): string {
-        return this._address;
-    }
-
-    constructor(address: string, PROTOCOL_NAME: string, webSocket: WebSocketClient.client) {
-        super();
-
-        this._address = address;
-        this.setTransport(this);
-
-        this._webSocket = webSocket;
-
-        this._webSocket.on("connect", (connection) => {
-            this._webSocketConnection = connection;
-
-            this._webSocketConnection.on('message', (message) => {
-                this.readMessageData(message.utf8Data);
-            });
-
-            this._webSocketConnection.on('open', () => {
-                this.onOpen.emit(event);
-            });
-
-            this._webSocketConnection.on('error', (error) => {
-                this.onError.emit(null);
-            });
-
-            this._webSocketConnection.addListener('close', (code: number, desc: string) => {
-                this.onClose.emit(null);
-            });            
-        });
-        this._webSocket.connect(address, [PROTOCOL_NAME]);
-    }
-
-    public writeMessageData(data: string) {
-        this._webSocketConnection.send(data);
-    }
-
-    public getApi(): WebSocketConnectionAPI {
-        var api = <WebSocketConnectionAPI>super.getApi();
-        api.onOpen = this.onOpen;
-        api.onError = this.onError;
-        api.onClose = this.onClose;
-        api.close = this.close.bind(this);
-        return api;
-    }
-
-    public close(): void {
-        this._webSocketConnection.close();
-    }
-
-    static create(address: string, options: {
-        PROTOCOL_NAME?: string;
-    } = {}): WebSocketConnectionAPI {
-        var PROTOCOL_NAME = options.PROTOCOL_NAME || protocol.PROTOCOL_NAME;
-        var webSocket = new WebSocketClient.client();
-        var connection = new NodeWebSocketConnection(address, PROTOCOL_NAME, webSocket);
-        return connection.getApi();
-    }
-}
-
-export class BrowserWebSocketConnection extends connection.Connection {
+export class WebSocketConnection extends connection.Connection {
 
     private _address: string;
     private _webSocket: WebSocket;
@@ -137,10 +65,11 @@ export class BrowserWebSocketConnection extends connection.Connection {
 
     static create(address: string, options: {
         PROTOCOL_NAME?: string;
+        ws?: typeof WebSocket;
     } = {}): WebSocketConnectionAPI {
         var PROTOCOL_NAME = options.PROTOCOL_NAME || protocol.PROTOCOL_NAME;
-        var webSocket = new WebSocket(address, PROTOCOL_NAME);
-        var connection = new BrowserWebSocketConnection(address, webSocket);
+        var webSocket = new options.ws(address, PROTOCOL_NAME);
+        var connection = new WebSocketConnection(address, webSocket);
         return connection.getApi();
     }
 }
@@ -149,8 +78,9 @@ export function create(address: string, options: {
     PROTOCOL_NAME?: string;
 } = {}): WebSocketConnectionAPI {
     if (global.navigator) {
-        return BrowserWebSocketConnection.create(address, options);
+        (<any>options).ws = WebSocket;
     } else {
-        return NodeWebSocketConnection.create(address, options);
+        (<any>options).ws = require('websocket').w3cwebsocket;
     }
+    return WebSocketConnection.create(address, options);
 }
